81184cd063d0d787d1971ed50ce4037fb9b4964a
Krishna Kandula <krishna.c.kandula@gmail.com>
2016-11-21 10:08:43 -0600

    Began using lodash

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-var user = os.userInfo('username');
-fs.appendFile('greetings.txt', `Hello ${user.username}!, you are ${notes.age}`);
+//console.log(_.isString(true));
+//console.log(_.isString('krishna'));
+var filteredArray = _.uniq(['Krishna', 'Krishna', 1]);
+console.log(filteredArray);
+
+//console.log('Result:', notes.add(1, 2));
+//var user = os.userInfo('username');
+//fs.appendFile('greetings.txt', `Hello ${user.username}!, you are ${notes.age}`);
